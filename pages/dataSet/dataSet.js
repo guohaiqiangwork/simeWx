191699bb76a4e652065727dc402ac354d0498cff
老杜322 <491670004@qq.com>
2020-03-13 17:51:23 +0800

    个人设置资料页面

--- pages/dataSet/dataSet.js
+++ pages/dataSet/dataSet.js
@@ -8,6 +8,37 @@ Page({
     bar_Height: wx.getSystemInfoSync().statusBarHeight,
     ishideback: false,
     my_class: true,
+    setList: [{
+      name: '头像',
+      value: ''
+    },
+    {
+      name: '用户名',
+      value: '张三'
+    },
+    {
+      name: '性别',
+      value: '男',
+      modeVal: "selector",
+      index: 0,
+      sexArray: ['男', '女'],
+    },
+    {
+      name: '生日',
+      value: '1989-07-21',
+      modeVal: "date",
+      index: "",
+      sexArray: "",
+    }
+    ],
+    jnr: [{
+      name: '纪念日',
+      value: '07-21',
+      modeVal: "date",
+      index: "",
+      sexArray: ""
+    }]
+
   },
 
   /**
@@ -64,5 +95,43 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  //  点击时间组件确定事件  
+  bindTimeChange: function (e) {
+    console.log("谁哦按")
+    this.setData({
+      times: e.detail.value
+    })
+  },
+  //  点击日期组件确定事件  
+  bindDateChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      dates: e.detail.value 
+    })
+  },
+  //  点击城市组件确定事件  
+  bindPickerChange: function (e) {
+    let listIndex = e.currentTarget.dataset.index;
+    let listvalue = e.currentTarget.dataset.value;
+  
+    let sexFalg = e.detail.value;
+    let _this = this;
+    let list = "setList[" + listIndex + "].value"
+
+    if (listIndex == 2){
+      if (sexFalg == '0') {
+        _this.data.ni = '男'
+      } else {
+        _this.data.ni = '女'
+      }
+    }else{
+      _this.data.ni = sexFalg
+    }
+    
+    this.setData({
+      [list]: _this.data.ni
+    })
   }
 })

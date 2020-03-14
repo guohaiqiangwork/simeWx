d319d839c1a8b51848acedcef752867d31807355
guohaiqiang <837557495@qq.com>
2020-03-14 14:26:51 +0800

    分类处理

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,14 +1,91 @@
 // pages/category/category.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     bar_Height: wx.getSystemInfoSync().statusBarHeight,
     ishideback: true,
-    list: [{ name: '1' }, { name: '2' }, { name: '3' }, { name: '1' }],
-    rightList: { auctionMateriaList: [{ name: '32' }, { name: '32' }, { name: '32' }, { name: '32' }]}
+    my_class: false,
+    tabFalg: '001',
+    leftList: [{
+      name: '面点速食',
+      id: "001"
+    }, {
+      name: '酒水饮料',
+      id: "002"
+    }, {
+      name: '美妆百货',
+      id: "003"
+    }, {
+      name: '粮油干货',
+      id: "004"
+    }, {
+      name: '中外茶叶',
+      id: "005"
+    }, {
+      name: '母婴保健',
+      id: "006"
+    }, {
+      name: '家装家纺',
+      id: "007"
+    }, {
+      name: '日用纸品',
+      id: "008"
+    }, {
+      name: '电子数码',
+      id: "009"
+    }, {
+      name: '健康体检',
+      id: "010"
+    }],
+    rightList: [{
+      name: '面点速食',
+      picUrl: "/image/switch/2.jpg",
+      id: '001'
+    },
+    {
+      name: '酒水饮料',
+      picUrl: "/image/switch/1.jpg",
+      id: '002'
+    },
+    {
+      name: '粮油干货',
+      picUrl: "/image/switch/3.jpg",
+      id: '003'
+    },
+    {
+      name: '美妆百货',
+      picUrl: "/image/switch/2.jpg",
+      id: '004'
+    },
+    {
+      name: '中外茶叶',
+      picUrl: "/image/switch/2.jpg",
+      id: '005'
+    },
+    {
+      name: '母婴保健',
+      picUrl: "/image/switch/1.jpg",
+      id: '006'
+    },
+    {
+      name: '家装家纺',
+      picUrl: "/image/switch/2.jpg",
+      id: '007'
+    },
+    {
+      name: '日用纸品',
+      picUrl: "/image/switch/1.jpg",
+      id: '008'
+    },
+    {
+      name: '电子数码',
+      picUrl: "/image/switch/2.jpg",
+      id: '009'
+    }
+
+    ],
   },
 
   /**
@@ -65,5 +142,11 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 左边切换
+  swichTab: function (e) {
+    this.setData({
+      tabFalg: e.currentTarget.dataset.id
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,58 +6,251 @@ Page({
   data: {
     bar_Height: wx.getSystemInfoSync().statusBarHeight,
     ishideback: true,
-    banners: [
-      { businessId: '1', picUrl: "/image/switch/1.jpg" },
-      { businessId: '2', picUrl: "/image/switch/2.jpg" },
+    banners: [{
+        businessId: '1',
+        picUrl: "/image/switch/1.jpg"
+      },
+      {
+        businessId: '2',
+        picUrl: "/image/switch/2.jpg"
+      },
       {
         businessId: '1',
         picUrl: "http://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png"
       },
-      { businessId: '2', picUrl: "/image/switch/3.jpg" }
+      {
+        businessId: '2',
+        picUrl: "/image/switch/3.jpg"
+      }
     ], //轮播数组
-    tabList: [
-      { name: '面点速食', picUrl: "/image/switch/2.jpg" },
-      { name: '酒水饮料', picUrl: "/image/switch/1.jpg" },
-      { name: '粮油干货', picUrl: "/image/switch/3.jpg" },
-      { name: '美妆百货', picUrl: "/image/switch/2.jpg" },
-      { name: '中外茶叶', picUrl: "/image/switch/2.jpg" },
-      { name: '母婴保健', picUrl: "/image/switch/1.jpg" },
-      { name: '家装家纺', picUrl: "/image/switch/2.jpg" },
-      { name: '日用纸品', picUrl: "/image/switch/1.jpg" },
-      { name: '电子数码', picUrl: "/image/switch/2.jpg" }
-    
-    ],//导航栏数组
-    newList:[
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg:'极甜凤梨甜到心里',id:'001'},
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg: '极甜凤梨甜到心里', id: '002'},
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg: '极甜凤梨甜到心里', id: '003' },
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg: '极甜凤梨甜到心里', id: '004'},
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg: '极甜凤梨甜到心里', id: '005'},
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg: '极甜凤梨甜到心里', id: '006'},
-      { naem: '海南凤梨（1个装）', picUrl: '/image/switch/2.jpg', price: '13.00', falg: '极甜凤梨甜到心里' },
-    ],//新品发现
+    tabList: [{
+        name: '面点速食',
+        picUrl: "/image/switch/2.jpg",
+        id: '001'
+      },
+      {
+        name: '酒水饮料',
+        picUrl: "/image/switch/1.jpg",
+        id: '002'
+      },
+      {
+        name: '粮油干货',
+        picUrl: "/image/switch/3.jpg",
+        id: '003'
+      },
+      {
+        name: '美妆百货',
+        picUrl: "/image/switch/2.jpg",
+        id: '004'
+      },
+      {
+        name: '中外茶叶',
+        picUrl: "/image/switch/2.jpg",
+        id: '005'
+      },
+      {
+        name: '母婴保健',
+        picUrl: "/image/switch/1.jpg",
+        id: '006'
+      },
+      {
+        name: '家装家纺',
+        picUrl: "/image/switch/2.jpg",
+        id: '007'
+      },
+      {
+        name: '日用纸品',
+        picUrl: "/image/switch/1.jpg",
+        id: '008'
+      },
+      {
+        name: '电子数码',
+        picUrl: "/image/switch/2.jpg",
+        id: '009'
+      }
+
+    ], //导航栏数组
+    newList: [{
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里',
+        id: '001'
+      },
+      {
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里',
+        id: '002'
+      },
+      {
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里',
+        id: '003'
+      },
+      {
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里',
+        id: '004'
+      },
+      {
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里',
+        id: '005'
+      },
+      {
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里',
+        id: '006'
+      },
+      {
+        naem: '海南凤梨（1个装）',
+        picUrl: '/image/switch/2.jpg',
+        price: '13.00',
+        falg: '极甜凤梨甜到心里'
+      },
+    ], //新品发现
+    proudeList: [{
+        title: '酒水饮料',
+        id: "002",
+        data: [{
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '001'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '002'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '003'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '004'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '005'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '006'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里'
+          },
+        ]
+      },
+      {
+        title: '面点素食',
+        id: "001",
+        data: [{
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '001'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '002'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '003'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '004'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '005'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里',
+            id: '006'
+          },
+          {
+            naem: '海南凤梨（1个装）',
+            picUrl: '/image/switch/2.jpg',
+            price: '13.00',
+            falg: '极甜凤梨甜到心里'
+          },
+        ]
+      }
+
+
+
+    ],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  goSet:function(){
+  goSet: function() {
     wx.setStorage({
       key: 'key',
       data: 'sdfjakl是东方就是快乐'
     })
   },
-  getSet:function(){
-   const value = wx.getStorageSync('key');
+  getSet: function() {
+    const value = wx.getStorageSync('key');
     console.log(value)
   },
   //事件处理函数
-  bindViewTap: function () {
+  bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -85,7 +278,7 @@ Page({
       })
     }
   },
-  getUserInfo: function (e) {
+  getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -94,18 +287,27 @@ Page({
     })
   },
   // 去搜素页面
-  bindconfirm:function(e){
+  bindconfirm: function(e) {
     console.log(e.detail.value);
     wx.navigateTo({
       url: '/pages/searchResult/searchResult?value=' + e.detail.value,
     })
   },
   // 去产品详情
-  goProudctDetails: function (e) {
+  goProudctDetails: function(e) {
     console.log(e.currentTarget.dataset.productid);
     wx.navigateTo({
       url: '/pages/prouctDetails/prouctDetails?productId=' + e.currentTarget.dataset.productid,
     })
   },
-  
+
+  // 点击导航页面滚动
+  toViewClick: function(e) {
+    console.log(e.currentTarget.dataset.id);
+    wx.pageScrollTo({
+      scrollTop: 600,
+      duration: 300
+    });
+  }
+
 })

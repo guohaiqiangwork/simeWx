1d72596f10a8d9d5d70146b05ae33c7b7ebf27d3
guohaiqiang <837557495@qq.com>
2020-03-16 18:17:22 +0800

    地址管理

--- pages/address/address.js
+++ pages/address/address.js
@@ -8,61 +8,184 @@ Page({
     bar_Height: wx.getSystemInfoSync().statusBarHeight,
     ishideback: false,
     my_class: true,
+    region: ['请选择'],
+    address_list: [1, 2, 3],
+
+
+    delBtnWidth: 160,
+    data: [{
+      content: "1",
+      right: 0
+    }, {
+      content: "2",
+      right: 0
+    }, {
+      content: "3",
+      right: 0
+    }, {
+      content: "4",
+      right: 0
+    }, {
+      content: "5",
+      right: 0
+    }, {
+      content: "6",
+      right: 0
+    }, {
+      content: "7",
+      right: 0
+    }, {
+      content: "8",
+      right: 0
+    }, {
+      content: "9",
+      right: 0
+    }, {
+      content: "10",
+      right: 0
+    }],
+    isScroll: true,
+    windowHeight: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          windowHeight: res.windowHeight
+        });
+      }
+    });
+  },
+  drawStart: function(e) {
+    // console.log("drawStart");  
+    var touch = e.touches[0]
+
+    for (var index in this.data.data) {
+      var item = this.data.data[index]
+      item.right = 0
+    }
+    this.setData({
+      data: this.data.data,
+      startX: touch.clientX,
+    })
 
   },
+  // 滑动事件
+  drawMove: function(e) {
+    console.log(e)
+    var touch = e.touches[0]
+    var item = this.data.data[e.currentTarget.dataset.index]
+    var disX = this.data.startX - touch.clientX
+    if (disX >= 20) {
+      if (disX > this.data.delBtnWidth) {
+        disX = this.data.delBtnWidth
+      }
+      item.right = disX
+      this.setData({
+        isScroll: false,
+        data: this.data.data
+      })
+    } else {
+      item.right = 0
+      this.setData({
+        isScroll: true,
+        data: this.data.data
+      })
+    }
+  },
+  // 滑动事件
+  drawEnd: function(e) {
+    var item = this.data.data[e.currentTarget.dataset.index]
+    if (item.right >= this.data.delBtnWidth / 2) {
+      item.right = this.data.delBtnWidth
+      this.setData({
+        isScroll: true,
+        data: this.data.data,
+      })
+    } else {
+      item.right = 0
+      this.setData({
+        isScroll: true,
+        data: this.data.data,
+      })
+    }
+  },
+
+  delItem: function(e) {
+
+  },
+
+
+
+
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
-  }
+  },
+  //地址点击确定按钮
+  bindRegionChange: function(e) {
+    console.log(e)
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  },
+  // 开关默认地址
+  switch2Change: function(e) {
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  },
+
+
 })

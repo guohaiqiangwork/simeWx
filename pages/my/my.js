f56639ff7a034cc3bd145504bf615ddebb6e204e
guohaiqiang <837557495@qq.com>
2020-03-19 15:52:05 +0800

    我的页面数据

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,127 +1,137 @@
 // pages/my/my.js
+const app = getApp()
+const ajax_url = app.globalData.ajax_url;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     bar_Height: wx.getSystemInfoSync().statusBarHeight,
     ishideback: true,
     name: '留一手',
-    hiddenName: false
+    hiddenName: false,
+    myData: ''
   },
-  getUserMessage: function () {
-    //alert("获取个人信息")
-    getUserDetail(mui, plus.storage.getItem('memberId'), function (data) {
-      if (data.code == 200) {
-       console.log(JSON.stringify(data))
-      } else if (data.code == 401 || data.code == 1500) {
-        //alert("需要登录");
-        //gotoLoginIn()
-      } else {
-        tipShow(data.message)
-        $("#code").val('')
-      }
-    });
-  },
-  // 获取门店列表
-  GetList: function (that) {
-    this.setData({
-      refresh_falg: false
-    });
-    wx.showLoading({
-      title: '加载中',
-    })
-    wx.request({
-      url: ajax_url + '/mb/find/'+'',
-      method: "POST",
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',// 默认值
-        'Authorization': "Bearer" + " " + plus.storage.getItem('Token'),
-        'client': 'APP',
-      },
-      data: {
-     
-      },
-      success: function (res) {
-        wx.hideLoading();
-        that.setData({
-          refresh_falg: true
-        });
-      }
-    });
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getMyDeatail() //获取个人信息
+    this.getMoney()//获取余额
   },
-
+  // 获取个人信息
+  getMyDeatail: function() {
+    var _this = this;
+    wx.request({
+      url: ajax_url + '/mb/find/' + wx.getStorageSync('useId'),
+      method: "get",
+      header: {
+        'Authorization': "Bearer" + " " + wx.getStorageSync('token'),
+        'client': 'APP',
+      },
+      success: function(res) {
+        if (res.data.code == '200') {
+          _this.setData({
+            myData: res.data.data
+          })
+        } else {
+          wx.showModal({
+            content: res.data.message,
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+        }
+      }
+    })
+  },
+  // 获取余额
+  getMoney: function () {
+    var _this = this;
+    wx.request({
+      url: ajax_url + '/account/find/' + wx.getStorageSync('useId'),
+      method: "get",
+      header: {
+        'Authorization': "Bearer" + " " + wx.getStorageSync('token'),
+        'client': 'APP',
+      },
+      success: function (res) {
+        if (res.data.code == '200') {
+          console.log(res)
+          _this.setData({
+            myMoney: res.data.data
+          })
+        } else {
+          wx.showModal({
+            content: res.data.message,
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
     console.log('离开时间')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
   // 我的订单页面跳转
-  goMyOrder: function (e) {
+  goMyOrder: function(e) {
     let falg = e.currentTarget.dataset.falg;
     wx.navigateTo({
       url: '../myOrder/myOrder?index=' + falg,
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { }
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {}
     })
   },
-// 去余额
-  goBalance:function(){
+  // 去余额
+  goBalance: function() {
     wx.navigateTo({
       url: '../myBalance/myBalance',
     })
   }
-  
+
 })

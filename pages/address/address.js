132b2aa07a4e953a8fec632aeabd1b9fc5f6dbda
guohaiqiang <837557495@qq.com>
2020-03-19 17:02:46 +0800

    地址处理

--- pages/address/address.js
+++ pages/address/address.js
@@ -1,23 +1,18 @@
-// pages/address/address.js
+const app = getApp()
+const ajax_url = app.globalData.ajax_url;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     bar_Height: wx.getSystemInfoSync().statusBarHeight,
     ishideback: false,
     my_class: true,
+    title:'收货地址',
     region: ['请选择'],
-    items: [{
-      name: '1'
-    }, {
-      name: '1'
-    }, {
-      name: '1'
-    }],
+    items: [],
     startX: 0, //开始坐标
-    startY: 0
+    startY: 0,
+    receiver:'',
+    mobile:'',
+    addAddressId:''
   },
 
   /**
@@ -25,14 +20,233 @@ Page({
    */
   onLoad: function(options) {
     console.log(this.data.items.length)
-    for (var i = 0; i < this.data.items.length; i++) {
-      this.data.items[i].isTouchMove = false
-    }
-    console.log(this.data.items)
+    // for (var i = 0; i < this.data.items.length; i++) {
+    //   this.data.items[i].isTouchMove = false
+    // }
+    // console.log(this.data.items)
+    // this.setData({
+    //   items: this.data.items
+    // });
+
+    this.getAddressList() //获取地址列表
+  },
+  // 监听输入框
+  nameInput: function(e) {
+    var _this = this;
+    _this.setData({
+      receiver: e.detail.value
+    })
+  },
+  phoneInput: function(e) {
+    var _this = this;
+    _this.setData({
+      mobile: e.detail.value
+    })
+  },
+  addresInput: function(e) {
+    var _this = this;
+    _this.setData({
+      address: e.detail.value
+    })
+  },
+  //列表点击新加地址
+  newAddAddress:function(){
     this.setData({
-      items: this.data.items
+      items:[],
+      title:'添加地址'
     });
   },
+// 地址编辑
+  editAddress:function(e){
+    console.log(e.currentTarget.dataset.item)
+    var editList = e.currentTarget.dataset.item;
+    if (editList.isDefault == 1){
+      editList.isDefault = true
+    }else{
+      editList.isDefault = false
+    }
+    this.setData({
+      items: [],
+      address: editList.address,
+      mobile: editList.mobile,
+      receiver: editList.receiver,
+      region: [editList.province, editList.city, editList.area],
+      isDefault: editList.isDefault,
+      addAddressId: editList.id,
+      title: '修改地址'
+    });
+  },
+  // 新增地址
+  addAddress: function() {
+    var _this = this;
+    if (!_this.data.receiver) {
+      wx.showModal({
+        content: '请输入收货人姓名',
+        confirmColor: '#6928E2',
+        showCancel: false,
+      })
+      return;
+    } else if (_this.data.mobile.length < 11 || !(/^1[3456789]\d{9}$/.test(_this.data.mobile))) {
+      wx.showModal({
+        content: '请填写正确手机号',
+        confirmColor: '#6928E2',
+        showCancel: false,
+      })
+      return;
+    } else if (!_this.data.address) {
+      wx.showModal({
+        content: '请填写详细地址',
+        confirmColor: '#6928E2',
+        showCancel: false,
+      })
+      return;
+    } else if (_this.data.region.length == 1) {
+      wx.showModal({
+        content: '请选择所在区域',
+        confirmColor: '#6928E2',
+        showCancel: false,
+      })
+      return;
+    }
+    var data = {
+      receiver: _this.data.receiver, //收货人
+      mobile: _this.data.mobile, //收货人联系电话
+      address: _this.data.address, //具体地址
+      memberId: wx.getStorageSync('useId'), //会员主键id
+      province: _this.data.region[0], //省
+      city: _this.data.region[1], //城市
+      area: _this.data.region[2], //区域
+      id: _this.data.addAddressId, //地址主键id
+      isDefault: _this.data.isDefault, //	是否默认
+    }
+  // 判断是新增还是修改
+  if(_this.data.title == '修改地址'){
+    wx.request({
+      url: ajax_url + '/address/updateAddress',
+      method: "post",
+      data: data,
+      header: {
+        'Authorization': "Bearer" + " " + wx.getStorageSync('token'),
+        'client': 'APP',
+      },
+      success: function (res) {
+        if (res.data.code == '200') {
+          wx.showModal({
+            content: '修改成功',
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+          _this.getAddressList();//获取列表
+        } else {
+          wx.showModal({
+            content: res.data.message,
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+        }
+      }
+    })
+  }else{
+    wx.request({
+      url: ajax_url + '/address/save',
+      method: "post",
+      data: data,
+      header: {
+        'Authorization': "Bearer" + " " + wx.getStorageSync('token'),
+        'client': 'APP',
+      },
+      success: function (res) {
+        if (res.data.code == '200') {
+          wx.showModal({
+            content: '添加成功',
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+          _this.getAddressList();//获取列表
+        } else {
+          wx.showModal({
+            content: res.data.message,
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+        }
+      }
+    })
+  }
+
+  
+  },
+  // 获取地址列表
+  getAddressList: function() {
+    var _this = this;
+    wx.request({
+      url: ajax_url + '/address/findAll/' + wx.getStorageSync('useId'),
+      method: "get",
+      header: {
+        'Authorization': "Bearer" + " " + wx.getStorageSync('token'),
+        'client': 'APP',
+      },
+      success: function(res) {
+        if (res.data.code == '200') {
+          console.log(res)
+          if (res.data.data.length > 0) {
+           var addressList = res.data.data;
+            for (var i = 0; i < addressList.length; i++) {
+              addressList.isTouchMove = false
+            }
+          }
+          _this.setData({
+            items: addressList
+          })
+        } else {
+          wx.showModal({
+            content: res.data.message,
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+        }
+      }
+    })
+
+  },
+  //删除事件
+  del: function (e) {
+    wx.showLoading({
+      title: '删除中...',
+    })
+    var _this = this;
+    wx.request({
+      url: ajax_url + '/address/deleteAddress/' + e.currentTarget.dataset.id,
+      method: "post",
+      header: {
+        'Authorization': "Bearer" + " " + wx.getStorageSync('token'),
+        'client': 'APP',
+      },
+      success: function (res) {
+        wx.hideLoading();
+        if (res.data.code == '200') {
+          wx.showModal({
+            content: '删除成功',
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+          _this.getAddressList();//刷新列表
+        } else {
+          wx.showModal({
+            content: res.data.message,
+            confirmColor: '#6928E2',
+            showCancel: false,
+          })
+        }
+      }
+    })
+
+    // this.data.items.splice(e.currentTarget.dataset.index, 1)
+    // this.setData({
+    //   items: this.data.items
+    // })
+  },
+
   //手指触摸动作开始 记录起点X坐标
   touchstart: function(e) {
     //开始触摸时 重置所有删除
@@ -89,13 +303,7 @@ Page({
     //返回角度 /Math.atan()返回数字的反正切值
     return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
   },
-  //删除事件
-  del: function(e) {
-    this.data.items.splice(e.currentTarget.dataset.index, 1)
-    this.setData({
-      items: this.data.items
-    })
-  },
+ 
   //跳转
   goDetail() {
     console.log('点击元素跳转')
@@ -160,6 +368,15 @@ Page({
   // 开关默认地址
   switch2Change: function(e) {
     console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+    if (e.detail.value) {
+      this.setData({
+        isDefault: 1
+      })
+    } else {
+      this.setData({
+        isDefault: 2
+      })
+    }
   },
 
 
